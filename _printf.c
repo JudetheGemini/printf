693fc86d42af2b203b876aa63229cafe51ac789a
Jude Ubuntu <root@JUDE.localdomain>
2022-05-20 11:09:45 +0100

    first test

---
+++ _printf.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+ * _printf - print arguments to output stream and return number of arguments
+ * @format: character string
+ * Return: number of arguments entered
+ */
+int _printf(const char *format, ...)
+{
+	va_list str;
+	char c, *s;
+	const char specifiers[] = "cs%";
+	
+	va_copy(format, specifiers); 
+	va_start(str, format);
+
+	while (*format)
+		switch(*format++){
+		case 'c':
+			c = (char) va_arg(str, int);
+			return (write (1, &c, 1));
+			break;
+		case 's':
+			s = va_arg(str, char *);
+			return (write(1, &s, 1));
+			break;
+		}
+	va_end(str);
+} 
---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef _HEADER_FILE_
+#define _HEADER_FILE_
+
+#include <stdarg.h>
+int _printf(const char *format, ...);
+
+#endif
